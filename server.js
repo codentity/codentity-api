2bff35af5a3f165a503b3fc24cafec010239994e
Jack Stevens <jack@applyr.com>
2015-10-07 03:08:12 +0100

    Improve landing page

--- server.js
+++ server.js
@@ -2,12 +2,26 @@
 
 let Hapi = require('hapi');
 let server = new Hapi.Server();
+let packageJson = require('./package');
 
 server.connection({
   port: process.env.PORT || 5000,
 });
 
 require('./packages/packages.router')(server);
+
+server.route({
+  path: '/',
+  method: 'GET',
+  handler: function (request, reply) {
+    reply({
+      name: packageJson.name,
+      message: packageJson.description,
+      version: packageJson.version,
+      packagesUrl: `${request.server.info.protocol}://${request.info.host}/packages`
+    });
+  }
+});
 
 server.start(function (err) {
   console.log('Server listening on', server.info.uri);
