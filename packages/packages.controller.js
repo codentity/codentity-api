6f1b4573c34324c1c36cff11ce1b2c9dca19ff6e
Jack Stevens <jack@applyr.com>
2015-10-07 02:12:21 +0100

    Use a different host from Hapi

--- packages/packages.controller.js
+++ packages/packages.controller.js
@@ -5,14 +5,14 @@ let Github = require('../lib/Github');
 module.exports = {
   getImage: function (request, reply) {
     let github = new Github({
-      baseUrl: request.server.info.uri
+      baseUrl: request.info.host
     });
     var imageUrl = github.getImageUrl(request.params.id);
     reply.redirect(imageUrl);
   },
   getPackageInfo: function (request, reply) {
     let github = new Github({
-      baseUrl: request.server.info.uri
+      baseUrl: request.info.host
     });
     github.getPackageConfig(request.params.id)
     .then(function (config) {
@@ -24,8 +24,9 @@ module.exports = {
     });
   },
   list: function (request, reply) {
+    console.log(request.info.host);
     let github = new Github({
-      baseUrl: request.server.info.uri
+      baseUrl: request.info.host
     });
     github.getPackages()
     .then(function (packages) {
